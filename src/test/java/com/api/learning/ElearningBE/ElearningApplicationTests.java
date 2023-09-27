6cc97a40bede74aa2a59a3545d63b203408d3498
TanPhuc-GH <truongtanphuc2002@gmail.com>
2023-09-28 03:01:55 +0700

    first commit

---
+++ src/test/java/com/api/learning/ElearningBE/ElearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.learning.ElearningBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
