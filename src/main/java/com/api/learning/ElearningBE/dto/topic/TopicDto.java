1558095730e68c3766253ed9c908ff3953d2a1f1
TanPhuc-GH <truongtanphuc2002@gmail.com>
2023-12-19 00:36:16 +0700

    Add createDate and modifiedDate into TopicDto

--- src/main/java/com/api/learning/ElearningBE/dto/topic/TopicDto.java
+++ src/main/java/com/api/learning/ElearningBE/dto/topic/TopicDto.java
@@ -5,6 +5,7 @@ import com.api.learning.ElearningBE.dto.forum.ForumDto;
 import com.api.learning.ElearningBE.dto.topic_comment.TopicCommentAdminDto;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -13,5 +14,7 @@ public class TopicDto {
     private String topicContent;
     private ForumDto forumInfo;
     private AccountDto accountInfo;
+    private Date createDate;
+    private Date modifiedDate;
     List<TopicCommentAdminDto> commentInfo;
 }
